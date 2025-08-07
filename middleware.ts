c11b0e2223d9325944d8c3bab2abbb79f6518a30
nihatonal <onalnihat1986@gmail.com>
2025-08-07 17:18:47 +0300

    middleware

--- middleware.ts
+++ middleware.ts
@@ -4,9 +4,7 @@ import { routing } from "./i18n/routing";
 export default createMiddleware(routing);
 
 export const config = {
-  // Match only internationalized pathnames
   matcher: [
-    // Ana sayfa ve dil path'leriyle eşleş, ancak bazı statik dosyaları hariç tut
-    "/((?!_next|favicon.ico|manifest.json|icons|robots.txt|sitemap.xml).*)",
+    "/((?!_next|favicon.ico|manifest.json|icons|robots.txt|sitemap.xml|images|fonts).*)",
   ],
 };
