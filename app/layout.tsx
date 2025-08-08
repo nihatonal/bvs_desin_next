13cc81a68ad4ec48c4c0076b34a46dcc05c1affc
nihatonal <onalnihat1986@gmail.com>
2025-08-08 09:17:03 +0300

    GA mulk fixed error

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,24 +12,28 @@ export default function RootLayout({
                 <link rel="icon" href="/favicon.ico" sizes="any" />
                 <meta name="theme-color" content="#9F8CFD" />
                 <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
+
+            </head>
+            <body>
+                {children}
+
                 {/* Google Analytics: gtag.js */}
                 <Script
                     src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
                     strategy="afterInteractive"
                 />
-                <Script id="google-analytics" strategy="afterInteractive">
+                <Script
+                    id="google-analytics" strategy="afterInteractive">
                     {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
-              page_path: window.location.pathname,
-            });
-          `}
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
+                    page_path: window.location.pathname,
+                    });
+                    `}
+
                 </Script>
-            </head>
-            <body>
-                {children}
             </body>
         </html>
     );
